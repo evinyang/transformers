fbbf76dd3e15dcf8063547a569a6b4bd827c3ace
evinyang <e@evinyang.com>
2023-05-31 13:09:08 -0700

    Update modeling_speecht5.py

--- src/transformers/models/speecht5/modeling_speecht5.py
+++ src/transformers/models/speecht5/modeling_speecht5.py
@@ -1371,7 +1371,7 @@ class SpeechT5Decoder(SpeechT5PreTrainedModel):
     def _prepare_decoder_attention_mask(self, attention_mask, input_shape, inputs_embeds, past_key_values_length):
         # create causal mask
         # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-        combined_attention_mask = None
+        combined_attention_mask: Optiona[torch.Tensor] = None
         if input_shape[-1] > 1:
             combined_attention_mask = _make_causal_mask(
                 input_shape[0], input_shape[1],
