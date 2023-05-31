f0cacc5923d85d82fc1d3191bf97ec9ce70b3fd0
evinyang <e@evinyang.com>
2023-05-31 15:33:14 -0700

    Update modeling_speecht5.py

--- src/transformers/models/speecht5/modeling_speecht5.py
+++ src/transformers/models/speecht5/modeling_speecht5.py
@@ -1833,9 +1833,9 @@ class SpeechT5Model(SpeechT5PreTrainedModel):
     @add_start_docstrings_to_model_forward(SPEECHT5_INPUTS_DOCSTRING)
     def forward(
         self,
-        input_values: torch.Tensor,
-        attention_mask: Optional[torch.LongTensor],
-        decoder_input_values: torch.Tensor,
+        input_values: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.LongTensor] = None,
+        decoder_input_values: Optional[torch.Tensor] = None,
         decoder_attention_mask: Optional[torch.LongTensor] = None,
         head_mask: Optional[torch.FloatTensor] = None,
         decoder_head_mask: Optional[torch.FloatTensor] = None,
